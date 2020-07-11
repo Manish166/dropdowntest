01101589150fa046adb8f3c6bdd9f309f727c836
Manish166 <manish@superadmins-MacBook-Pro.local>
2020-07-11 23:11:02 +0800

    added Add&Sel, addcoountry

--- src/App.js
+++ src/App.js
@@ -6,42 +6,57 @@ import data from './data'
 const App=()=> {
   const [searchValue, setSearchValue] = useState('')
   const [countries, setCountries] = useState(data.countries)
-  const [display, setDisplay] = useState(false)
-  const [moreToView, setMoreToView] = useState(0)
-
+  const [displayDrop, setDisplayDrop] = useState(false)
+  const [displayViewMore, setDisplayViewMore] = useState(true)
+  const [displayAddButton, setDisplayAddButton] = useState(false)
+  const optionsLimit=7
+  const moreToView = data.countries.length-optionsLimit
   useEffect(()=>{
     console.log('useEffect')
     if (searchValue.length>2){
-      const filteredCountries = countries.filter(country=>country.countryName.toLowerCase().includes(searchValue))
+      const filteredCountries = data.countries.filter(country=>country.countryName.toLowerCase().includes(searchValue.toLowerCase()))
       setCountries(filteredCountries)
-    }
+      if (filteredCountries.length==0){
+        setDisplayAddButton(true)
+      } else {
+        setDisplayAddButton(false)
+      }
+      setDisplayViewMore(false)
+    }           
     if (searchValue.length<=2){
       const countriesToIterate = data.countries.slice(1, optionsLimit)
-      setMoreToView(data.countries.length- optionsLimit)
       setCountries(countriesToIterate)
+      setDisplayViewMore(true)
     }
   },[searchValue])
-  const optionsLimit=5
 
   const handleOnChange = (e)=>{
     setSearchValue(e.target.value)
   }
 
   const toggle= ()=>{
-    display ? setDisplay(false) : setDisplay(true)
+    displayDrop ? setDisplayDrop(false) : setDisplayDrop(true)
   }
 
-  const showAll = ()=>{
+  const handleViewMoreClick = ()=>{
     setCountries(data.countries)
+    setDisplayViewMore(false)
+  }
+  const addCountry=()=>{
+    let country = {"countryId" : 0, "countryName" : ""}
+    country.countryId=data.countries.length +1
+    country.countryName=searchValue
+    data.countries.push(country)
   }
   return (
     <div>
       <button onClick={toggle}>Select a Location</button>
-      {display && 
+      {displayDrop && 
         <div>
-        <SearchBar value={searchValue} handleOnChange={handleOnChange}/>
-        <DropdownList countries={countries} moreToView={moreToView}/>
-        <p onClick={showAll}>{moreToView} more</p>
+          <SearchBar value={searchValue} handleOnChange={handleOnChange}/>
+          <DropdownList countries={countries}/>
+          {displayViewMore && <p onClick={handleViewMoreClick}>{moreToView} more</p>}
+          {displayAddButton && <button onClick={addCountry}>Add and Select</button>}
         </div>
       }
     </div>
